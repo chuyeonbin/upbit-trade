dc4aefc99f702f5d5798a788efd78628e59bba9f
ChuYeonBin <cndusqls98@gmail.com>
2022-09-19 01:44:16 +0900

    feat: 시세 체결 조회 api 구현

--- src/api/index.ts
+++ src/api/index.ts
@@ -17,13 +17,19 @@ export async function getCandleByDays(market: string, count: number = 1) {
 }
 
 export async function getCandleByWeeks(market: string, count: number = 1) {
-  const query = `candles/weeks?market=${market}&count=${count}`;
+  const query = `/candles/weeks?market=${market}&count=${count}`;
   const response = await axios.get(BASE_URL + query);
   return response.data;
 }
 
 export async function getCandleByMonths(market: string, count: number = 1) {
-  const query = `candles/months?market=${market}&count=${count}`;
+  const query = `/candles/months?market=${market}&count=${count}`;
+  const response = await axios.get(BASE_URL + query);
+  return response.data;
+}
+
+export async function getRecentContract(market: string, count: number = 1) {
+  const query = `trades/ticks?market=${market}&count=${count}`;
   const response = await axios.get(BASE_URL + query);
   return response.data;
 }
