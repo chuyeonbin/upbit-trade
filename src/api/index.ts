f8837c524c45aa79c3005a3858f684ed97d026bb
ChuYeonBin <cndusqls98@gmail.com>
2022-09-19 17:50:51 +0900

    fix: api 호출 함수 count, unit  인자 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,31 +8,31 @@ export async function getMarketCodes() {
   return response.data;
 }
 
-export async function getCandleByMinutes(market: string, unit: number = 1, count: number = 1) {
+export async function getCandleByMinutes(market: string, unit: number, count: number) {
   const query = `/candles/minutes/${unit}?market=KRW-BTC&count=${count}`;
   const response = await axios.get(BASE_URL + query);
   return response.data;
 }
 
-export async function getCandleByDays(market: string, count: number = 1) {
+export async function getCandleByDays(market: string, count: number) {
   const query = `/candles/days?market=${market}&count=${count}`;
   const response = await axios.get(BASE_URL + query);
   return response.data;
 }
 
-export async function getCandleByWeeks(market: string, count: number = 1) {
+export async function getCandleByWeeks(market: string, count: number) {
   const query = `/candles/weeks?market=${market}&count=${count}`;
   const response = await axios.get(BASE_URL + query);
   return response.data;
 }
 
-export async function getCandleByMonths(market: string, count: number = 1) {
+export async function getCandleByMonths(market: string, count: number) {
   const query = `/candles/months?market=${market}&count=${count}`;
   const response = await axios.get(BASE_URL + query);
   return response.data;
 }
 
-export async function getRecentContract(market: string, count: number = 1) {
+export async function getRecentContract(market: string, count: number) {
   const query = `/trades/ticks?market=${market}&count=${count}`;
   const response = await axios.get(BASE_URL + query);
   return response.data;
