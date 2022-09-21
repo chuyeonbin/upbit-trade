6088df71fe3828fd45cc235238aff60f849cadea
ChuYeonBin <cndusqls98@gmail.com>
2022-09-21 16:48:40 +0900

    feat: redux-saga init

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from './rootReducer';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: [sagaMiddleware],
+  devTools: true,
 });
+
+sagaMiddleware.run(rootSaga);
+
 export type RootState = ReturnType<typeof store.getState>;
 
 export default store;
