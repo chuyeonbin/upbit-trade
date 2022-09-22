f00865ad4f309dcc39418a3039de54dea32c1b3c
ChuYeonBin <cndusqls98@gmail.com>
2022-09-22 21:36:27 +0900

    feat: socket connection state 타입 정의

--- src/types/state.ts
+++ src/types/state.ts
@@ -1,1 +1,11 @@
-export type RootState = {};
+export interface SocketState {
+  socket: WebSocket | null;
+
+  socketConnectionLoading: boolean;
+  socketConnectionDone: boolean;
+  socketConnectionError: null;
+}
+
+export interface RootState {
+  socket: SocketState;
+}
