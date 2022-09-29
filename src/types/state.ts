37ceeb7b6e3fc7949c24719e83b431334d268dbb
ChuYeonBin <cndusqls98@gmail.com>
2022-09-29 16:44:17 +0900

    feat: marketList state type 정의

--- src/types/state.ts
+++ src/types/state.ts
@@ -1,9 +1,20 @@
+import { MarketCodes } from '.';
+
 export interface SocketState {
   socketConnectionLoading: boolean;
   socketConnectionSuccess: boolean;
   socketConnectionError: Error | null;
 }
 
+export interface CoinState {
+  marketList: MarketCodes;
+
+  loadMarketListLoading: boolean;
+  loadMarketListSuccess: boolean;
+  loadMarketListError: Error | null;
+}
+
 export default interface RootState {
   socket: SocketState;
+  coin: CoinState;
 }
