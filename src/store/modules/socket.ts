e33ee54d602da3c4025a2d6f36d11156254957e3
ChuYeonBin <cndusqls98@gmail.com>
2022-09-29 16:45:10 +0900

    chore: 불필요한 코드 제거

--- src/store/modules/socket.ts
+++ src/store/modules/socket.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { SocketState } from '../../types/state';
 
 const initialState: SocketState = {
