daf18a5c61f85ac3146e8f474b491c89a83acf90
ChuYeonBin <cndusqls98@gmail.com>
2022-09-29 17:02:14 +0900

    fix: rootReducer 수정 & useAppSelector 추가

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,13 +1,10 @@
-import { combineReducers, AnyAction } from '@reduxjs/toolkit';
-import RootState from '../types/state';
+import { combineReducers } from '@reduxjs/toolkit';
 import socketReducer from './modules/socket';
 import coinReducer from './modules/coin';
 
-const rootReducer = (state: RootState, action: AnyAction): RootState => {
-  return combineReducers({
-    socket: socketReducer,
-    coin: coinReducer,
-  })(state, action);
-};
+const rootReducer = combineReducers({
+  socket: socketReducer,
+  coin: coinReducer,
+});
 
 export default rootReducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,20 @@
-import { configureStore, Reducer, AnyAction } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
-import RootState from '../types/state';
 import rootReducer from './rootReducer';
 import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-  reducer: rootReducer as Reducer<RootState, AnyAction>,
+  reducer: rootReducer,
   middleware: [sagaMiddleware],
   devTools: true,
 });
 
 sagaMiddleware.run(rootSaga);
+
+export type RootState = ReturnType<typeof store.getState>;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
