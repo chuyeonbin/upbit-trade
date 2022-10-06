d51454e5c837e4aaa970e668d9ef69428b2f63f1
ChuYeonBin <cndusqls98@gmail.com>
2022-10-06 18:55:58 +0900

    feat: 체결가 소켓 연결 리듀서 구현

--- src/store/modules/socket.ts
+++ src/store/modules/socket.ts
@@ -1,17 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { SocketState } from '../../types/state';
 
 const initialState: SocketState = {
   presentPriceSocketLoading: false,
   presentPriceSocketDone: false,
   presentPriceSocketError: null,
+
+  tradeSocketLoading: false,
+  tradeSocketDone: false,
+  tradeSocketError: null,
 };
 
 const socketSlice = createSlice({
   initialState,
   name: 'socket',
   reducers: {
-    presentPriceSocketRequest: (state) => {
+    presentPriceSocketRequest: (state, { payload }: PayloadAction<string[]>) => {
       state.presentPriceSocketLoading = true;
       state.presentPriceSocketDone = false;
       state.presentPriceSocketError = null;
@@ -24,10 +28,29 @@ const socketSlice = createSlice({
       state.presentPriceSocketLoading = false;
       state.presentPriceSocketError = payload.error;
     },
+    tradeSocketRequest: (state, { payload }: PayloadAction<string[]>) => {
+      state.tradeSocketLoading = true;
+      state.tradeSocketDone = false;
+      state.tradeSocketError = null;
+    },
+    tradeSocketSuccess: (state) => {
+      state.tradeSocketLoading = false;
+      state.tradeSocketDone = true;
+    },
+    tradeSocketFailure: (state, { payload }) => {
+      state.tradeSocketLoading = false;
+      state.tradeSocketError = payload.error;
+    },
   },
 });
 
-export const { presentPriceSocketRequest, presentPriceSocketSuccess, presentPriceSocketFailure } =
-  socketSlice.actions;
+export const {
+  presentPriceSocketRequest,
+  presentPriceSocketSuccess,
+  presentPriceSocketFailure,
+  tradeSocketRequest,
+  tradeSocketSuccess,
+  tradeSocketFailure,
+} = socketSlice.actions;
 
 export default socketSlice.reducer;
