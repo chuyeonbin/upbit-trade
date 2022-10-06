ec77949d788069036fdf7e9e71315a2715f0eb97
ChuYeonBin <cndusqls98@gmail.com>
2022-10-06 17:57:11 +0900

    fix: 매개변수 action creator들이 dispatch 되게 수정

--- src/store/sagas/socket.ts
+++ src/store/sagas/socket.ts
@@ -1,15 +1,17 @@
-import { PayloadAction } from '@reduxjs/toolkit';
+import { ActionCreatorWithoutPayload, ActionCreatorWithPayload } from '@reduxjs/toolkit';
 import { END, EventChannel, eventChannel } from 'redux-saga';
-import { all, call, fork, put, take, takeEvery } from 'redux-saga/effects';
+import { call, put, take } from 'redux-saga/effects';
 import { createSocket } from '../../utils/socket';
-import { presentPriceSocketFailure, presentPriceSocketRequest } from '../modules/socket';
 
-function channelConnection(): EventChannel<string> {
+function channelConnection(field: {
+  type: 'ticker' | 'trade' | 'orderbook';
+  codes: string[];
+}): EventChannel<string> {
   return eventChannel((emitter) => {
     const ws = createSocket();
 
     ws.onopen = () => {
-      const msg = JSON.stringify([{ ticket: 'upbit' }, { type: 'trade', codes: ['KRW-BTC'] }]);
+      const msg = JSON.stringify([{ ticket: 'upbit' }, { type: field.type, codes: ['KRW-BTC'] }]);
 
       ws.send(msg);
     };
@@ -40,27 +42,30 @@ function closeChannel(channel: EventChannel<string> | null) {
   }
 }
 
-function* socketConnection({ payload }: PayloadAction<any>) {
-  console.log(payload);
+export function* socketConnection(
+  field: {
+    type: 'ticker' | 'trade' | 'orderbook';
+    codes: string[];
+  },
+  action: {
+    request: ActionCreatorWithoutPayload<string>;
+    success: ActionCreatorWithoutPayload<string>;
+    failure: ActionCreatorWithPayload<any, string>;
+  },
+) {
   let channel: EventChannel<string>;
   try {
-    channel = yield call(channelConnection);
+    yield put(action.request());
+    channel = yield call(channelConnection, field);
+    yield put(action.success());
     while (true) {
       const msg: string = yield take(channel);
       console.log(msg);
     }
   } catch (error) {
     console.error(error);
-    yield put(presentPriceSocketFailure(error));
+    yield put(action.failure(error));
   } finally {
     closeChannel(channel!);
   }
-}
-
-function* watchPresentPriceSocket() {
-  yield takeEvery(presentPriceSocketRequest, socketConnection);
-}
-
-export default function* socketSaga() {
-  yield all([fork(watchPresentPriceSocket)]);
 }
