c091611688ffab6660b8e53057496ca87a1fd46d
ChuYeonBin <cndusqls98@gmail.com>
2022-11-10 16:24:55 +0900

    feat: OrderbookHeader tab 클릭한 탭 색 변경

--- src/components/OrderbookHeader/index.tsx
+++ src/components/OrderbookHeader/index.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 export default function OrderbookHeader() {
+  const [selectedTab, setSelectedTab] = useState<number>(0);
+
+  const handleTabClick = (index: number) => setSelectedTab(index);
+
   const tabList = ['일반호가', '누적호가', '호가주문'];
   return (
     <Wrapper>
       <Tabs>
         {tabList.map((tab, index) => (
-          <Tab key={index}>{tab}</Tab>
+          <Tab key={tab} onClick={() => handleTabClick(index)} selected={selectedTab === index}>
+            {tab}
+          </Tab>
         ))}
       </Tabs>
     </Wrapper>
@@ -19,10 +26,14 @@ const Tabs = styled.ul`
   display: flex;
 `;
 
-const Tab = styled.li`
+const Tab = styled.li<{ selected: boolean }>`
   flex-grow: 1;
   height: 45px;
   line-height: 45px;
   text-align: center;
   font-weight: 600;
+  color: ${({ selected }) => (selected ? '#1976d2' : 'black')};
+  border-bottom: ${({ selected }) => (selected ? '3px' : '1px')} solid
+    ${({ selected }) => (selected ? '#1976d2' : '#d5d6dc')};
+  cursor: pointer;
 `;
