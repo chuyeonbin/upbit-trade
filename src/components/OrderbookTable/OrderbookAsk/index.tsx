8edb9f10f21ce94112977ac8fb502589667cc86f
ChuYeonBin <cndusqls98@gmail.com>
2022-11-14 16:38:17 +0900

    feat: 현재가와 맞는 셀 border 표시하기

--- src/components/OrderbookTable/OrderbookAsk/index.tsx
+++ src/components/OrderbookTable/OrderbookAsk/index.tsx
@@ -11,7 +11,7 @@ interface OrderbookAskProps {
 }
 
 export default function OrderbookAsk({ index, askPrice, askSize, maxSize }: OrderbookAskProps) {
-  const { prevClosingPrice } = useAppSelector((state) => state.coin.selectedCoin);
+  const { prevClosingPrice, tradePrice } = useAppSelector((state) => state.coin.selectedCoin);
   return (
     <TableRow>
       <OrderbookAskCell sx={{ width: '42px' }} />
@@ -28,6 +28,7 @@ export default function OrderbookAsk({ index, askPrice, askSize, maxSize }: Orde
             {dayToDayFormat(askPrice - prevClosingPrice, prevClosingPrice)}%
           </p>
         </a>
+        {tradePrice === askPrice ? <Seleceted /> : null}
       </OrderbookAskCell3>
       {index === 0 ? (
         <Inner1 rowSpan={15} colSpan={2}>
@@ -65,6 +66,10 @@ const OrderbookAskCell2 = styled(OrderbookAskCell)`
 `;
 
 const OrderbookAskCell3 = styled(OrderbookAskCell)<{ change: number }>`
+  && {
+    position: relative;
+  }
+
   & > a {
     width: 150px;
     display: flex;
@@ -80,6 +85,28 @@ const Bar = styled.div`
   height: 26px;
   background-color: rgba(18, 97, 196, 0.14901960784313725);
   max-width: 100%;
+`;
+
+const Seleceted = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border: 2px solid #333;
+
+  ::after {
+    position: absolute;
+    top: 50%;
+    left: 0;
+    transform: translateY(-50%);
+    width: 0;
+    height: 0;
+    border-color: transparent transparent transparent #333;
+    border-style: solid;
+    border-width: 6px 0 6px 6px;
+    content: '';
+  }
 `;
 
 const Inner1 = styled(TableCell)`
--- src/components/OrderbookTable/OrderbookBid/index.tsx
+++ src/components/OrderbookTable/OrderbookBid/index.tsx
@@ -11,7 +11,7 @@ interface OrderbookBidProps {
 }
 
 export default function OrderbookBid({ index, bidPrice, bidSize, maxSize }: OrderbookBidProps) {
-  const { prevClosingPrice } = useAppSelector((state) => state.coin.selectedCoin);
+  const { prevClosingPrice, tradePrice } = useAppSelector((state) => state.coin.selectedCoin);
 
   return (
     <TableRow>
@@ -27,6 +27,7 @@ export default function OrderbookBid({ index, bidPrice, bidSize, maxSize }: Orde
             {dayToDayFormat(bidPrice - prevClosingPrice, prevClosingPrice)}%
           </p>
         </a>
+        {tradePrice === bidPrice ? <Seleceted /> : null}
       </OrderbookBidCell2>
       <OrderbookBidCell3 sx={{ width: '120px' }} align='right'>
         <a href='#'>
@@ -51,6 +52,9 @@ const OrderbookBidCell = styled(TableCell)`
 `;
 
 const OrderbookBidCell2 = styled(OrderbookBidCell)<{ change: number }>`
+  && {
+    position: relative;
+  }
   & > a {
     width: 150px;
     display: flex;
@@ -81,6 +85,28 @@ const Bar = styled.div`
   height: 26px;
   background-color: rgba(200, 74, 49, 0.14901960784313725);
   max-width: 100%;
+`;
+
+const Seleceted = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border: 2px solid #333;
+
+  ::after {
+    position: absolute;
+    top: 50%;
+    left: 0;
+    transform: translateY(-50%);
+    width: 0;
+    height: 0;
+    border-color: transparent transparent transparent #333;
+    border-style: solid;
+    border-width: 6px 0 6px 6px;
+    content: '';
+  }
 `;
 
 const Inner2 = styled(TableCell)`
