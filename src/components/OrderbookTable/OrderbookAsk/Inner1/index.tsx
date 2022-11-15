09e76b0a909a49387d174ef87959ac76a5d7f70d
ChuYeonBin <cndusqls98@gmail.com>
2022-11-15 19:27:14 +0900

    style: #12 inner1 component style 구현

--- src/components/OrderbookTable/OrderbookAsk/Inner1/index.tsx
+++ src/components/OrderbookTable/OrderbookAsk/Inner1/index.tsx
@@ -4,40 +4,42 @@ import { TableCell } from '@mui/material';
 export default function Inner1() {
   return (
     <Inner rowSpan={15} colSpan={2}>
-      <dl>
-        <dt>거래량</dt>
-        <dd>8,615BTC</dd>
-        <dt>거래 대금</dt>
-        <dd>
-          195,525<i>백만원</i>
-          <em>(최근24시간)</em>
-        </dd>
-      </dl>
-      <dl>
-        <dt>52주 최고</dt>
-        <dd>
-          80,250,000<em>(2021.11.15)</em>
-        </dd>
-        <dt>52주 최저</dt>
-        <dd>
+      <Dl>
+        <Dt>거래량</Dt>
+        <Dd>
+          8,615<I>BTC</I>
+        </Dd>
+        <Dt>거래 대금</Dt>
+        <Dd>
+          195,525<I>백만원</I>
+          <Em>(최근24시간)</Em>
+        </Dd>
+      </Dl>
+      <Dl>
+        <Dt>52주 최고</Dt>
+        <Up>
+          80,250,000<Date>(2021.11.15)</Date>
+        </Up>
+        <Dt>52주 최저</Dt>
+        <Down>
           21,509,000
-          <em>(2022.11.14)</em>
-        </dd>
-      </dl>
-      <dl>
-        <dt>전일 종가</dt>
-        <dd>22,701,000</dd>
-        <dt>당일 고가</dt>
-        <dd>
+          <Date>(2022.11.14)</Date>
+        </Down>
+      </Dl>
+      <Dl>
+        <Dt>전일 종가</Dt>
+        <Dd>22,701,000</Dd>
+        <Dt>당일 고가</Dt>
+        <Up>
           23,078,000
-          <em>+1.66%</em>
-        </dd>
-        <dt>당일 저가</dt>
-        <dd>
+          <Em>+1.66%</Em>
+        </Up>
+        <Dt>당일 저가</Dt>
+        <Down>
           22,530,000
-          <em>-0.75%</em>
-        </dd>
-      </dl>
+          <Em>-0.75%</Em>
+        </Down>
+      </Dl>
     </Inner>
   );
 }
@@ -46,5 +48,60 @@ const Inner = styled(TableCell)`
   && {
     padding: 0;
     vertical-align: bottom;
+    border-bottom: none;
+  }
+`;
+
+const Dl = styled.dl`
+  margin: 0 10px;
+  padding: 10px 0;
+  overflow: hidden;
+  font-size: 12px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.lightGray};
+
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+
+const Dt = styled.dt`
+  float: left;
+  clear: both;
+  color: ${({ theme }) => theme.colors.darkGray};
+`;
+
+const Dd = styled.dd`
+  float: right;
+`;
+
+const Up = styled(Dd)`
+  color: ${({ theme }) => theme.colors.lightRed};
+  & > em {
+    color: ${({ theme }) => theme.colors.lightRed};
+  }
+`;
+
+const Down = styled(Dd)`
+  color: ${({ theme }) => theme.colors.lightBlue};
+  & > em {
+    color: ${({ theme }) => theme.colors.lightBlue};
+  }
+`;
+
+const I = styled.i`
+  font-style: normal;
+  color: ${({ theme }) => theme.colors.darkGray};
+`;
+
+const Em = styled.em`
+  display: block;
+  text-align: right;
+  color: ${({ theme }) => theme.colors.darkGray};
+  font-style: normal;
+`;
+
+const Date = styled(Em)`
+  && {
+    color: ${({ theme }) => theme.colors.darkGray};
   }
 `;
