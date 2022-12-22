f9ea6f990145a4a37e6a68195d4935d86e754cfd
ChuYeonBin <cndusqls98@gmail.com>
2022-12-22 23:25:49 +0900

    feat: 한글 퍼지 검색 함수 구현

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -37,3 +37,48 @@ export function createSocket(): WebSocket {
 
   return ws;
 }
+
+export function createFuzzyMatcher(input: string) {
+  const pattern = input.split('').map(chToPattern).join('.*?');
+  return new RegExp(pattern);
+}
+
+export function chToPattern(ch: string) {
+  if (/[ㄱ-ㅎ]/.test(ch)) {
+    const chToBegin: {
+      [ch: string]: number;
+    } = {
+      ㄱ: '가'.charCodeAt(0),
+      ㄲ: '까'.charCodeAt(0),
+      ㄴ: '나'.charCodeAt(0),
+      ㄷ: '다'.charCodeAt(0),
+      ㄸ: '따'.charCodeAt(0),
+      ㄹ: '라'.charCodeAt(0),
+      ㅁ: '마'.charCodeAt(0),
+      ㅂ: '바'.charCodeAt(0),
+      ㅃ: '빠'.charCodeAt(0),
+      ㅅ: '사'.charCodeAt(0),
+      ㅆ: '싸'.charCodeAt(0),
+      ㅇ: '아'.charCodeAt(0),
+      ㅈ: '자'.charCodeAt(0),
+      ㅊ: '차'.charCodeAt(0),
+      ㅋ: '카'.charCodeAt(0),
+      ㅌ: '타'.charCodeAt(0),
+      ㅍ: '파'.charCodeAt(0),
+      ㅎ: '하'.charCodeAt(0),
+    };
+
+    const begin = chToBegin[ch];
+    const end = begin + 587;
+    return `[${ch}\\u${begin.toString(16)}-\\u${end.toString(16)}]`;
+  } else if (/[가-히]/.test(ch)) {
+    const offset = '가'.charCodeAt(0);
+    const chCode = ch.charCodeAt(0) - offset;
+
+    if (chCode % 28 <= 0) {
+      const begin = Math.floor(chCode / 28) * 28 + offset;
+      const end = begin + 27;
+      return `[\\u${begin.toString(16)}-\\u${end.toString(16)}]`;
+    } else return ch;
+  }
+}
