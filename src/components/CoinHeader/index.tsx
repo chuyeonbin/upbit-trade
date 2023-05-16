1268c828c877f5a75617e55ee3e0e36e989271c7
ChuYeonBin <cndusqls98@gmail.com>
2023-05-16 17:08:35 +0900

    feat: HeaderRight component 구현

--- src/components/CoinHeader/index.tsx
+++ src/components/CoinHeader/index.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 import { useAppSelector } from '../../store/store';
-import { tradeVolume24hFormat } from '../../utils';
 import HeaderTitle from './HeaderTitle';
 import HeaderLeft from './HeaderLeft';
+import HeaderRight from './HeaderRight';
 
 export default function CoinHeader() {
   const selectedCoin = useAppSelector((state) => state.coin.selectedCoin);
+
   return (
     <HeaderWrapper>
       <HeaderTitle market={selectedCoin.code} koreanName={selectedCoin.marketName} />
@@ -15,31 +16,13 @@ export default function CoinHeader() {
           tradePrice={selectedCoin.tradePrice}
           prevClosingPrice={selectedCoin.prevClosingPrice}
         />
-        <PriceWrapper>
-          <PriceA>
-            <HP>
-              고가 <span>{selectedCoin.highPrice.toLocaleString()}</span>
-            </HP>
-            <LP>
-              저가 <span>{selectedCoin.lowPrice.toLocaleString()}</span>
-            </LP>
-          </PriceA>
-          <PriceB>
-            <ATV24H>
-              거래량(24H)
-              <span>
-                {tradeVolume24hFormat(selectedCoin.accTradeVolume24h)}{' '}
-                <i>{selectedCoin.code.substring(4)}</i>
-              </span>
-            </ATV24H>
-            <ATP24H>
-              거래대금(24H)
-              <span>
-                {Math.round(selectedCoin.accTradePrice24h).toLocaleString()} <i>KRW</i>
-              </span>
-            </ATP24H>
-          </PriceB>
-        </PriceWrapper>
+        <HeaderRight
+          highPrice={selectedCoin.highPrice}
+          lowPrice={selectedCoin.lowPrice}
+          accTradeVolume24h={selectedCoin.accTradeVolume24h}
+          accTradePrice24h={selectedCoin.accTradePrice24h}
+          market={selectedCoin.code}
+        />
       </HeaderBody>
     </HeaderWrapper>
   );
@@ -54,70 +37,4 @@ const HeaderBody = styled.div`
   padding: 18px 20px 14px;
   display: flex;
   justify-content: space-between;
-`;
-
-const PriceWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 500px;
-  font-size: ${({ theme }) => theme.fontSize.micro};
-`;
-
-const PriceA = styled.div`
-  margin-left: 16px;
-  flex-basis: 50%;
-`;
-
-const HP = styled.div`
-  padding-bottom: 11px;
-  display: flex;
-  justify-content: space-between;
-  border-bottom: 1px solid ${({ theme }) => theme.colors.lightGray};
-
-  & > span {
-    color: ${({ theme }) => theme.colors.lightRed};
-    font-weight: 500;
-  }
-`;
-
-const LP = styled.p`
-  padding-top: 11px;
-
-  display: flex;
-  justify-content: space-between;
-
-  & > span {
-    color: ${({ theme }) => theme.colors.lightBlue};
-    font-weight: 500;
-  }
-`;
-
-const PriceB = styled.div`
-  margin-left: 16px;
-  flex-basis: 50%;
-`;
-
-const ATV24H = styled.p`
-  padding-bottom: 11px;
-  display: flex;
-  justify-content: space-between;
-  border-bottom: 1px solid ${({ theme }) => theme.colors.lightGray};
-
-  & > span > i {
-    font-size: ${({ theme }) => theme.fontSize.microSmall};
-    color: ${({ theme }) => theme.colors.darkGray};
-  }
-`;
-
-const ATP24H = styled.p`
-  padding-top: 11px;
-  display: flex;
-  justify-content: space-between;
-  font-size: 12px;
-
-  & > span > i {
-    font-size: ${({ theme }) => theme.fontSize.microSmall};
-    color: ${({ theme }) => theme.colors.darkGray};
-  }
 `;
