be4f6eab02cbdd6daba8e7d6a9a1455f179d0d54
ChuYeonBin <cndusqls98@gmail.com>
2023-05-18 16:13:39 +0900

    feat: 선택된 코인 background 설정 구현

--- src/components/CoinItem/index.tsx
+++ src/components/CoinItem/index.tsx
@@ -1,4 +1,4 @@
-import { memo, useCallback } from 'react';
+import { memo, useCallback, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { changeSelectedCoin } from '../../store/modules/coin';
@@ -34,7 +34,12 @@ export default memo(function CoinItem({ market, koreanName, englishName, coin }:
   }, [marketName]);
 
   return (
-    <Wrapper onClick={handleCoinItemClick} change={coin.signedChangePrice} key={market}>
+    <Wrapper
+      onClick={handleCoinItemClick}
+      selected={marketName === koreanName}
+      change={coin.signedChangePrice}
+      key={market}
+    >
       <BookMarkCell />
       <TitleCell koreanName={koreanName} englishName={englishName} market={market} />
       <TradePriceCell tradePrice={coin.tradePrice} />
@@ -47,8 +52,9 @@ export default memo(function CoinItem({ market, koreanName, englishName, coin }:
   );
 });
 
-const Wrapper = styled.tr<{ change: number }>`
+const Wrapper = styled.tr<{ change: number; selected: boolean }>`
   height: 45px;
+  background-color: ${({ selected }) => (selected ? '#f4f5f8' : 'none')};
   border-bottom: 1px solid green;
 
   &:hover {
