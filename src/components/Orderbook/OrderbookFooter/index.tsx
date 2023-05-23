a2a0afec89c462b0d8c1e0c309ac65af6dc6dc23
ChuYeonBin <cndusqls98@gmail.com>
2023-05-23 21:33:01 +0900

    refactor: OrderbookFooter component mui 제거

--- src/components/Orderbook/OrderbookFooter/index.tsx
+++ src/components/Orderbook/OrderbookFooter/index.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { Table, TableBody, TableCell, TableRow } from '@mui/material';
 import { useAppSelector } from '../../../store/store';
 
 interface OrderbookFooterProps {
@@ -11,45 +10,59 @@ export default function OrderbookFooter({ totalAskSize, totalBidSize }: Orderboo
   const market = useAppSelector((state) => state.coin.selectedCoin.market);
 
   return (
-    <Wrapper>
-      <Table>
-        <TableBody>
-          <OrderbookFooterRow>
-            <OrderbookFooterCell align='right'>{totalAskSize.toFixed(3)}</OrderbookFooterCell>
-            <OrderbookFooterCell align='center'>
-              <strong>수량</strong>
-              <i>({market.substring(4)})</i>
-            </OrderbookFooterCell>
-            <OrderbookFooterCell align='left'>{totalBidSize.toFixed(3)}</OrderbookFooterCell>
-          </OrderbookFooterRow>
-        </TableBody>
-      </Table>
-    </Wrapper>
+    <Table>
+      <TableBody>
+        <TableRow>
+          <TableCell align='right'>{totalAskSize.toFixed(3)}</TableCell>
+          <TableCell align='center'>
+            <strong>수량</strong>
+            <i>({market.substring(4)})</i>
+          </TableCell>
+          <TableCell align='left'>{totalBidSize.toFixed(3)}</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
   );
 }
 
-const Wrapper = styled.div``;
+const Table = styled.table`
+  width: 100%;
+  border-spacing: 0;
+`;
 
-const OrderbookFooterRow = styled(TableRow)`
+const TableBody = styled.tbody``;
+
+const TableRow = styled.tr`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > td:nth-child(1) {
+    text-align: right;
+  }
+
   & > td:nth-child(2) {
+    text-align: center;
     border-left: 1px solid #f1f1f4;
     border-right: 1px solid #f1f1f4;
   }
+
+  & > td:nth-child(3) {
+    text-align: left;
+  }
 `;
 
-const OrderbookFooterCell = styled(TableCell)`
-  && {
-    padding: 0 8px;
-    width: 33.33333%;
-    border-top: 1px solid #f1f1f4;
-    border-bottom: 1px solid #f1f1f4;
-    height: 45px;
-    background-color: #f9fafc;
-    font-size: 12px;
-  }
+const TableCell = styled.td`
+  flex-grow: 1;
+  padding: 0 8px;
+  height: 45px;
+  line-height: 45px;
+  border-top: 1px solid #f1f1f4;
+  border-bottom: 1px solid #f1f1f4;
+  background-color: #f9fafc;
+  font-size: 12px;
 
   & > i {
     font-size: ${({ theme }) => theme.fontSize.microSmall};
-    font-style: none;
   }
 `;
